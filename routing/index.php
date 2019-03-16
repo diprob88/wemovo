d32501cc1802dca2ab7da181d0d59017a1accac0
Roberto <diprob88@gmail.com>
2019-03-16 23:37:57 +0100

    add form

--- routing/index.php
+++ routing/index.php
@@ -1,8 +1,48 @@
-<?php
-require_once 'src\php\class\Reader.php';
 
-$reader  =  new Reader();
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+                <div class="panel panel-info" >
+                    <div class="panel-heading">
+                        <div class="panel-title">Routes</div>
+                    </div>
 
+                    <div style="padding-top:30px" class="panel-body" >
 
+                        <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
 
-$reader->searchRoute(5,7);
+                        <form id="loginform" class="form-horizontal" role="form" action="api/routes.php" method="Get">
+
+                            <div style="margin-bottom: 25px" class="input-group">
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-road"></i></span>
+                                <input type="text" class="form-control" name="origin" value="" placeholder="Enter Origin">
+                            </div>
+
+                            <div style="margin-bottom: 25px" class="input-group">
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-road"></i></span>
+                                <input type="text" class="form-control" name="destination" value="" placeholder="Enter Destination">
+                            </div>
+                            
+                            <div style="margin-top:10px" class="form-group">
+                                <!-- Button -->
+
+                                <div class="col-sm-12 controls">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </div>
+
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
